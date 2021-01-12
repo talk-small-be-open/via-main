693550fbfbc3610c65c9aac0f08d1b264f9bd075
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-01-12 15:21:23 +0100

    peerjs

--- server/start_peerjs_server.sh
+++ server/start_peerjs_server.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # See https://github.com/peers/peerjs-server
-peerjs --port 9001 --key peerjs --path /peerjs --proxied true
+peerjs --port 9001 --key peerjs --path /peerjs --proxied true >> /var/log/peerjs.log
