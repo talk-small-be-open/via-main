6f99fca1e04ba426670b33fb65daf4e1a4495244
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-02-01 16:18:10 +0100

    deployment

--- server/maintenance_off.sh
+++ server/maintenance_off.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 sudo rm -f /etc/nginx/sites-enabled/000_maintenance
+sudo ln -s /etc/nginx/sites-available/via.vhost /etc/nginx/sites-enabled/
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
 sudo service nginx reload
--- server/maintenance_on.sh
+++ server/maintenance_on.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 sudo ln -s /etc/nginx/sites-available/000_maintenance /etc/nginx/sites-enabled/
+
+sudo rm -f /etc/nginx/sites-enabled/via.vhost
+sudo rm -f /etc/nginx/sites-enabled/default
+
 sudo service nginx reload
