8ba2165fbf81cef4657499ee7e73cd5beb6a2bc9
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2021-04-15 18:44:16 +0200

    progress

---
+++ server/scan_source_uitexts.sh
@@ -0,0 +1,13 @@
+#/bin/bash
+
+# Scans the source code for UI texts. NOTE: Does not find dynamically created UI text keys.
+# Use this list for a rough manual cleanup job in the via backend
+
+# Changing into GemStone git repository for our source code (base and site)
+pushd /opt/GsDevKit_home/shared/repos > /dev/null
+
+# Scan for UI-text patterns and list all found UI texts
+#egrep -r --no-filename --only-matching --include="*.st" "\'([a-zA-Z]+\:)+([a-zA-Z]+)\'" . | sed "s/'//g" | sort | uniq
+egrep -r --no-filename --only-matching --include="*.st" "'([[:alpha:]][[:alnum:]]*:)+([[:alpha:]][[:alnum:]]*)'" via* | sed "s/'//g" | sort | uniq
+
+popd > /dev/null
