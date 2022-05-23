da1ebffed9669cb9f19a843fd05072e547e9bc6a
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2022-05-23 17:39:02 +0200

    script

--- server/status_of_backup.sh
+++ server/status_of_backup.sh
@@ -27,16 +27,16 @@ done
 echo -e "GEMSTONE DATABASE\n================"
 TARGET=$BACKEND/via_db
 duplicity collection-status --verbosity warning $TARGET
-if [ "$VERBOSE"="true" ]; then duplicity list-current-files --verbosity warning $TARGET; fi
+[ "$VERBOSE" = "true" ] && duplicity list-current-files --verbosity warning $TARGET
 
 # Assets
 echo -e "ASSETS\n========"
 TARGET=$BACKEND/via_assets
 duplicity collection-status --verbosity warning $TARGET
-if [ "$VERBOSE"="true" ]; then duplicity list-current-files --verbosity warning $TARGET; fi
+[ "$VERBOSE" = "true" ] && duplicity list-current-files --verbosity warning $TARGET
 
 # Exported module PDFs and others
 echo -e "EXPORTS\n========"
 TARGET=$BACKEND/via_exports
 duplicity collection-status --verbosity warning $TARGET
-if [ "$VERBOSE"="true" ]; then duplicity list-current-files --verbosity warning $TARGET; fi
+[ "$VERBOSE" = "true" ] && duplicity list-current-files --verbosity warning $TARGET
