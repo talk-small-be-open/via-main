f00579a851e57513188286510b9d02c8a92fd6f9
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2023-06-08 20:55:13 +0200

    shrink script

--- server/shrink_stone.sh
+++ server/shrink_stone.sh
@@ -1,48 +1,51 @@
 #!/bin/bash
+set -e
+
+# TEST FIRST!
+exit
 
 # Verkleinert die GemStone Datenbank (shrink)
-
-# WICHTIG!!!!
-# Dieses Script ist noch nicht gut getestet. Nur manuell die einzelnen Schritte durchführen
-exit
 
 STONE=$1
 
 export GS_HOME=/opt/GsDevKit_home
 STONE_DIR=$GS_HOME/server/stones/$STONE
 BACKUPNAME="shrinking_gemstone.dbf"
-LOGFILE="shrink.log"
-rm -f $LOGFILE
 
 # Prohibit that the last temporary shrinked DB data file will be used accidentally. Delete it.
-rm -f $STONE_DIR/backups/$BACKUPNAME.gz
+BACKUPFILE=$STONE_DIR/backups/$BACKUPNAME.gz
+rm -f $BACKUPFILE
 
 # Stop all seaside processes
 ./monit_stop_web_gems.sh
-#./monit_stop_netldi.sh
 
 # OPTIMIZE: Synchronously wait for monit, but does not seem possible
-wait 20
-
 echo "Waiting until no topaz processes exists anymore ..."
-while pgrep topaz  > /dev/null; do sleep 1; echo -n "."; done
+sleep 3
+while pgrep topaz > /dev/null; do sleep 1; echo -n "."; done
 
 # object log leeren, oder zumindest die alten sachen!!!
-todeIt $STONE "ol clear --age=\`1 day\`" > $LOGFILE
+todeIt $STONE "ol clear --age=\`1 day\`"
 
 # GS Mark for collection
-todeIt $STONE "gs mfc" > $LOGFILE
+todeIt $STONE gs mfc
 
 # Backup machen
-todeBackup $STONE $BACKUPNAME > $LOGFILE
+#todeBackup $STONE $BACKUPNAME
+# We need the "wait" option, therefore via todeIt. Then, a preceding sleep is not necessary anymore.
+# TODO: --wait macht error, warum?
+#todeIt $STONE bu backup --commit --wait $BACKUPNAME
+todeIt $STONE bu backup --commit $BACKUPNAME
+
+
+# Just go for sure, since we had issues, and no backup file was created without any error
+[ -f "$BACKUPFILE" ] || (echo "no backup file created!" ; exit 1)
 
 # Stone und allfälliges netldi stoppen
 ./monit_stop_netldi.sh
 ./monit_stop_stone.sh
 
 # OPTIMIZE: Synchronously wait for monit, but does not seem possible
-wait 10
-
 echo "Waiting until no stoned processes exists anymore ..."
 while pgrep stoned > /dev/null; do sleep 1; echo -n "."; done
 
@@ -51,33 +54,39 @@ pushd $STONE_DIR/extents/
 mv --backup=numbered extent0.dbf _before_shrink_extent0.dbf
 popd
 
-# # -t oder -a? um die tode Struktur beizubehalten
-# newExtent -t -s $STONE_DIR/backups/$BACKUPNAME $STONE
-# # Oder?  todeIt ViaSiteTdu mount @/sys/stone/dirs/Seaside3/tode /home seaside
 
-# Alternativ, wegen blödem login disabled Fehler
+# HINWEIS:
+# falls kein oder ein vielzugrosses tode-snapshot snapshots/extent0.tode.dbf da ist, eins erstellen:
 # newExtent $STONE
 # todeLoad $STONE
-# todeIt $STONE "script --script=rebuildServerTode"
-# todeRestore $STONE $STONE_DIR/backups/shrinking_gemstone.dbf.gz
-# todeIt $STONE  mount @/sys/stone/dirs/Seaside3/tode /home seaside
+# stopStone $STONE
+# Behalten für später und umbenennen auf $STONE_DIR/snapshots/extent0.tode.pristine.dbf
+# extent brauchen wir nicht mehr: rm extent0.dbf
 
+[ -f "$STONE_DIR/snapshots/extent0.tode.pristine.dbf" ] || (echo "no pristine extent.dbf found" ; exit 1)
 
-# GEHT!!!
-# falls kein tode-snapshot da ist, eins erstellen:
-# takes the extent0.seaside.dbf as template
-#newExtent $STONE
-#todeLoad $STONE
+# Start with new fresh extent, tode already loaded
+newExtent -s $STONE_DIR/snapshots/extent0.tode.pristine.dbf -t $STONE
 
-newExtent -s $STONE_DIR/snapshots/extent0.tode.dbf -t $STONE
+sleep 10
+
+# Restore from backup file from above
 todeRestore $STONE $STONE_DIR/backups/$BACKUPNAME.gz
+
+sleep 10
+
+# Remount seaside in tode (Not sure if needed)
 todeIt $STONE mount @/sys/stone/dirs/Seaside3/tode /home seaside
 
-#wird nicht mehr gebraucht todeIt $STONE "script --script=rebuildServerTode"
-
-
-
-
+# Stop the stone (implicitly started from newExtent)
 stopStone -b $STONE
 
+sleep 5
+
 ./via_start.sh
+
+echo "Compressing the backup dbf-file to save space"
+pushd $STONE_DIR/extents/
+[ -f "_before_shrink_extent0.dbf.gz" ] && mv --backup=numbered _before_shrink_extent0.dbf.gz _before_shrink_extent0.dbf.gz.bak
+nice -n 10 gzip _before_shrink_extent0.dbf
+popd
